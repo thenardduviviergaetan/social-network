e26c38adec958a3dd1af7105ed2f7fc374b3a6eb
qdelooze <>
2024-03-22 15:51:52 +0100

    fix real time msg

--- services/frontend/app/ui/dashboard/chat.tsx
+++ services/frontend/app/ui/dashboard/chat.tsx
@@ -1,4 +1,4 @@
-'use client'
+"use client";
 
 import { useEffect } from "react";
 import { useState } from "react";
@@ -18,7 +18,7 @@ class Message {
     target: string,
     type_target: string,
     sender: string,
-    image: string
+    image: string,
   ) {
     this.msg_type = msg_type;
     this.content = content;
@@ -35,7 +35,7 @@ class Target {
   target: string;
   constructor(
     type_target: string,
-    target: string
+    target: string,
   ) {
     this.type_target = type_target;
     this.target = target;
@@ -54,7 +54,7 @@ class ConnBtn {
 class ListUser {
   tab: Array<ConnBtn>;
   constructor() {
-    this.tab = new Array;
+    this.tab = new Array();
   }
   new(username: string, uuid: string, online: boolean) {
     this.tab.push(new ConnBtn(username, uuid, online));
@@ -63,7 +63,7 @@ class ListUser {
 class ListMessage {
   tab: Array<Message>;
   constructor() {
-    this.tab = new Array;
+    this.tab = new Array();
   }
   new(
     msg_type: string,
@@ -71,79 +71,85 @@ class ListMessage {
     target: string,
     type_target: string,
     sender: string,
-    image: string) {
-    this.tab.push(new Message(
-      msg_type,
-      content,
-      target,
-      type_target,
-      sender,
-      image
-    ));
+    image: string,
+  ) {
+    this.tab.push(
+      new Message(
+        msg_type,
+        content,
+        target,
+        type_target,
+        sender,
+        image,
+      ),
+    );
   }
 }
 export default function Chat({ user }: { user: string | null }) {
-  let listUser = new ListUser()
-  let listMessage = new ListMessage()
-  const userUuid = user
-  const [content, setContent] = useState("")
-  const [socket, setsocket] = useState(Object)
-  const [userList, setUserList] = useState(listUser.tab)
-  const [groupList, setGroupList] = useState([])
-  const [messageList, setmessageList] = useState(listMessage.tab)
-  const [target, settarget] = useState(new Target("undefined", "undefined"))
+  let listUser = new ListUser();
+  let listMessage = new ListMessage();
+  const userUuid = user;
+  const [content, setContent] = useState("");
+  const [socket, setsocket] = useState(Object);
+  const [userList, setUserList] = useState(listUser.tab);
+  const [groupList, setGroupList] = useState([]);
+  const [messageList, setmessageList] = useState(listMessage.tab);
+  const [target, settarget] = useState(new Target("undefined", "undefined"));
   useEffect(() => {
-    let sc = new WebSocket('ws://localhost:8000/api/ws')
+    let sc = new WebSocket("ws://localhost:8000/api/ws");
     sc.onopen = (event) => {
-      console.log('WebSocket connection opened');
-      sc.send(userUuid ?? '');
+      console.log("WebSocket connection opened");
+      sc.send(userUuid ?? "");
     };
 
     sc.onmessage = (event) => {
-      let message
+      let message: any;
       try {
         message = JSON.parse(event.data);
-        console.log("Message send", message)
+        console.log("Message send", message);
       } catch {
-        console.error('WebSocket message error:', event.data);
+        console.error("WebSocket message error:", event.data);
       }
 
       switch (message.msg_type) {
-        case 'status':
-          listUser = new ListUser()
+        case "status":
+          listUser = new ListUser();
           // console.log('WebSocket status:', message.status);
           message.status
-            .filter((el: ConnBtn) => { return el.uuid != userUuid })
+            .filter((el: ConnBtn) => {
+              return el.uuid != userUuid;
+            })
             .forEach((el: ConnBtn) => {
               listUser.new(el.username, el.uuid, el.online);
-            })
+            });
           setUserList(listUser.tab);
           // console.log(userList);
           break;
-        case 'history':
+        case "history":
           listMessage = new ListMessage();
           break;
-        case 'chat':
-          listMessage.new(
-            message.msg_type,
-            message.content,
-            message.target,
-            message.type_target,
-            message.sender,
-            message.image
-          )
-          setmessageList(listMessage.tab)
+        case "chat":
+          setmessageList(prevMessage => [...prevMessage, 
+            new Message(
+              message.msg_type,
+              message.content,
+              message.target,
+              message.type_target,
+              message.sender,
+              message.image,
+              )
+            ]);
           break;
       }
-    }
+    };
 
     sc.onerror = (event) => {
-      console.error('WebSocket error:', event);
-    }
+      console.error("WebSocket error:", event);
+    };
 
     sc.onclose = (event) => {
-      console.log('WebSocket connection closed:', event);
-    }
+      console.log("WebSocket connection closed:", event);
+    };
     setsocket(sc);
     return () => {
       sc.close();
@@ -155,84 +161,97 @@ export default function Chat({ user }: { user: string | null }) {
       <div className="chanel-list">
         <h2>Users</h2>
         <div className="user">
-          {
-            userList.map((users, idx) => {
-              return (
-                <div key={idx}>
-                  <button id={users.username} onClick={
-                    (e) => {
-                      let t = new Target("user", users.uuid)
-                      settarget(t)
-                      socket.send(JSON.stringify(new Message(
+          {userList.map((users, idx) => {
+            return (
+              <div key={idx}>
+                <button
+                  id={users.username}
+                  onClick={(e) => {
+                    let t = new Target("user", users.uuid);
+                    settarget(t);
+                    socket.send(JSON.stringify(
+                      new Message(
                         "history",
                         "",
                         "user",
                         users.uuid,
                         userUuid ?? "",
-                        ""
-                      )))
-                    }
-                  }>{users.username}
-                    <svg className="notification" width="10" height="10">
-                      <circle cx="5" cy="5" r="5" fill={users.online ? "green" : "red"}></circle>
-                    </svg>
-                  </button>
-                </div>
-              )
-            })
-          }
+                        "",
+                      ),
+                    ));
+                  }}
+                >
+                  {users.username}
+                  <svg className="notification" width="10" height="10">
+                    <circle
+                      cx="5"
+                      cy="5"
+                      r="5"
+                      fill={users.online ? "green" : "red"}
+                    >
+                    </circle>
+                  </svg>
+                </button>
+              </div>
+            );
+          })}
         </div>
         <div className="group">
-          {
-            groupList?.map((group, idx) => {
-              return (
-                <div key={idx}>
-                  <button>
-                  </button>
-                </div>
-              )
-            })
-          }
+          {groupList?.map((group, idx) => {
+            return (
+              <div key={idx}>
+                <button>
+                </button>
+              </div>
+            );
+          })}
         </div>
       </div>
       <div className="chat-container">
         <div className="messages">
-          {
-            messageList.map((message, idx) => {
-              return (
-                <div key={idx}>
-                  <div className="message">{message.content}</div>
-                </div>
-              )
-            })
-          }
+          {messageList.map((message, idx) => {
+            return (
+              <div key={idx}>
+                <div className="message">{message.content}</div>
+              </div>
+            );
+          })}
         </div>
         <div className="sender">
           <form id="form-chat">
-            <input id="chat-text" type="text" value={content} onChange={(e) => {
-              setContent(e.target.value)
-            }}
-            ></input>
-            <input id="submit" type="submit" onClick={(e) => {
-              e.preventDefault();
-              if (target.target !== "undefined") {
-                // console.log(content, user)
-                let message = new Message(
-                  "chat",
-                  content,
-                  target?.target,
-                  target?.type_target,
-                  userUuid ?? '',
-                  ""
-                )
-                let msg = JSON.stringify(message)
-                setContent("")
-                socket.send(msg);
-              }
-            }}></input>
+            <input
+              id="chat-text"
+              type="text"
+              value={content}
+              onChange={(e) => {
+                setContent(e.target.value);
+              }}
+            >
+            </input>
+            <input
+              id="submit"
+              type="submit"
+              onClick={(e) => {
+                e.preventDefault();
+                if (target.target !== "undefined") {
+                  let message = new Message(
+                    "chat",
+                    content,
+                    target?.target,
+                    target?.type_target,
+                    userUuid ?? "",
+                    "",
+                  );
+                  let msg = JSON.stringify(message);
+                  setContent("");
+                  socket.send(msg);
+                }
+              }}
+            >
+            </input>
           </form>
         </div>
       </div>
     </div>
-  )
+  );
 }

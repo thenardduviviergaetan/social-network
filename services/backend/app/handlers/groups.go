a89dc8985b9305ef69225bc3441fab8bed67bf1b
aguillen <agustinguillen@hotmail.fr>
2024-03-22 20:19:32 +0100

    Importing userID and a few more informations

--- services/backend/app/handlers/groups.go
+++ services/backend/app/handlers/groups.go
@@ -18,15 +18,23 @@ func HandleCreateGroup(w http.ResponseWriter, r *http.Request) {
 	}
 
 	group := models.Groups{}
+	group.CreatorID = r.URL.Query().Get("UUID")
+
+	if group.CreatorID == "" {
+		http.Error(w, "Failed to create user, no user ID was received", http.StatusInternalServerError)
+		return
+	}
 
 	db := r.Context().Value("database").(*sql.DB)
+
 	if errDecode := json.NewDecoder(r.Body).Decode(&group); errDecode != nil {
 		http.Error(w, "Invalid request body", http.StatusBadRequest)
 		return
 	}
 
 	if errCreate := groups.CreateGroup(db, &group); errCreate != nil {
-		http.Error(w, "Failed to create user", http.StatusInternalServerError)
+		fmt.Println("error", errCreate)
+		http.Error(w, "Failed to create group", http.StatusInternalServerError)
 		return
 	}
 
@@ -44,16 +52,38 @@ func HandleGetGroupList(w http.ResponseWriter, r *http.Request) {
 	db := r.Context().Value("database").(*sql.DB)
 
 	q := r.URL.Query()
+	created := q.Get("created")
+	member := q.Get("member")
 	userUUID := q.Get("user")
 	limit, _ := strconv.Atoi(q.Get("limit"))
 	page, _ := strconv.Atoi(q.Get("page"))
 	offset := (page - 1) * limit
+	finalList := []models.Groups{}
 
-	groupList, errGetGroups := groups.GetGroupsWhereUserIsMember(db, userUUID, limit, offset)
-	if errGetGroups != nil {
-		fmt.Println("ERROR =", errGetGroups)
-		http.Error(w, "Failed to retrieve the groups", http.StatusInternalServerError)
+	//TODO Find a way to not have copies of groups as a creator is also a member.
+
+	if created == "true" {
+		createdList, errGetGroups := groups.GetGroupsCreatedByUser(db, userUUID, limit, offset)
+		if errGetGroups != nil {
+			fmt.Println("ERROR =", errGetGroups)
+			http.Error(w, "Failed to retrieve the groups created by user", http.StatusInternalServerError)
+		}
+
+		finalList = append(finalList, createdList...)
 	}
 
-	json.NewEncoder(w).Encode(groupList)
+	if member == "true" {
+		memberList, errGetGroups := groups.GetGroupsWhereUserIsMember(db, userUUID, limit, offset)
+		// groupList, errGetGroups := groups.GetGroupsWhereUserIsMember(db, userUUID, limit, offset)
+		if errGetGroups != nil {
+			fmt.Println("ERROR =", errGetGroups)
+			http.Error(w, "Failed to retrieve the groups user is a member of", http.StatusInternalServerError)
+		}
+
+		fmt.Println("memberList = ", memberList)
+
+		finalList = append(finalList, memberList...)
+	}
+
+	json.NewEncoder(w).Encode(finalList)
 }

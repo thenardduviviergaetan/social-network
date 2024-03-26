990dccd6f332438ed58d42a58b3c9b69f83975c3
qdelooze <>
2024-03-26 16:37:12 +0100

    fix toggle

--- services/frontend/app/dashboard/groups/group/page.tsx
+++ services/frontend/app/dashboard/groups/group/page.tsx
@@ -3,17 +3,12 @@ import { Group, User } from "@/app/lib/definitions";
 import JoinGroupButton from "@/app/ui/groups/join";
 import { GroupMembers } from "@/app/ui/groups/group-members";
 import InviteComponent from "@/app/ui/groups/invite";
-import { useState } from "react";
-import Manager from "@/app/ui/groups/manager";
-import { API_BASE_URL } from "@/app/lib/constants"
-import { fetchGroup, fetchUser } from "@/app/lib/data"
-import {Group, User } from "@/app/lib/definitions"
-import EventCard from "@/app/ui/groups/event-card"
-import CreateEvent from "@/app/ui/groups/event-form"
-import JoinGroup from "@/app/ui/groups/join"
-import axios from "axios"
-import Image from "next/image"
-import Link from "next/link"
+import EventCard from "@/app/ui/groups/event-card";
+import CreateEvent from "@/app/ui/groups/event-form";
+import JoinGroup from "@/app/ui/groups/join";
+import Image from "next/image";
+import Link from "next/link";
+import { CADDY_URL } from "@/app/lib/constants";
 
 export default async function Page(
   {
@@ -29,84 +24,41 @@ export default async function Page(
   const user = await fetchUser();
   const group = await fetchGroup(searchParams?.id) as Group;
 
-    return (
-        <div className="text-white">
-            <div className="bg-white shadow-xl h-[80px] w-auto rounded-lg p-4 flex flex-row mb-3 justify-between">
-                <p className=" text-purple-700 rounded-lg p-3 w-4/12 font-bold">{group.name}</p>
-                <p className="  text-purple-700 rounded-lg p-3 w-4/12 font-bold">Owned by : <span>{group.creator_first_name} {group.creator_last_name}</span></p>
-            </div>
-            <div className="bg-white shadow-xl h-[80px] w-6/12 rounded-lg p-4">
-                <div className=" text-purple-700 rounded-lg p-3 w-auto font-bold">{group.description}</div>
-            </div>
-            <CreateEvent group={group} user={user?.uuid}/>
-            <div className="bg-white shadow-lg w-6/12 rounded-lg mt-8 p-5"><p className="text-purple-700 font-bold underline">Members of this group :</p>
-                <JoinGroup
-                    group={group}
-                    user={user?.uuid}
-                />
-
-                {group.members.map((member: User, index) => {
-                    return (
-                        <div key={index}>
-                            <Link   
-                                href={{
-                                    pathname: "/dashboard/profile",
-                                    query: { user: encodeURIComponent(member.uuid) },
-                                }}
-                            >
-                                <div className=" flex flex-row w-auto h-auto shadow-lg bg-zinc-200 p-3 rounded-lg mb-2 mt-5 justify-start items-center">
-                                    <Image
-                                        src={`http://caddy:8000/api/avatar?id=${member.uuid}`}
-                                        alt="Profile Picture"
-                                        width={50}
-                                        height={200}
-                                        className="rounded-full shadow-xl"
-                                    />
-                                    <p className="text-purple-700 font-bold">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{member.firstName}&nbsp;{member.lastName}</p>
-                                </div>
-                            </Link>
-                        </div>
-                    )
-                })}
-            </div>
-            <div className="flex flex-wrap flex-row justify-around mt-8 bg-white rounded-lg shadow-lg w-full h-auto p-5">
-            {
-                //FIXME: no events
-                group.events?.map((event,index)=>{
-                    return(
-                        <EventCard event={event} key={index} user={user?.uuid} />
-                    )
-                })
-            }
-            </div>
-        </div>
-
-    )
   return (
-    <div>
-      <JoinGroupButton
-        group={group}
-        user={user?.uuid}
-      />
+    <div className="text-white">
+      <div>
+        <JoinGroupButton
+          group={group}
+          user={user?.uuid}
+        />
+        <CreateEvent group={group} user={user?.uuid} />
 
-
-      <div className="bg-white shadow-xl h-[80px] w-auto rounded-lg p-4 flex flex-row mb-3 justify-between">
-        <p className=" text-purple-700 rounded-lg p-3 w-4/12 font-bold">
-          {group.name}
-        </p>
-        <p className="  text-purple-700 rounded-lg p-3 w-4/12 font-bold">
-          Owned by :{" "}
-          <span>{group.creator_first_name} {group.creator_last_name}</span>
-        </p>
-      </div>
-      <div className="bg-white shadow-xl h-[80px] w-6/12 rounded-lg p-4">
-        <div className=" text-purple-700 rounded-lg p-3 w-auto font-bold">
-          {group.description}
+        <div className="bg-white shadow-xl h-[80px] w-auto rounded-lg p-4 flex flex-row mb-3 justify-between">
+          <p className=" text-purple-700 rounded-lg p-3 w-4/12 font-bold">
+            {group.name}
+          </p>
+          <p className="  text-purple-700 rounded-lg p-3 w-4/12 font-bold">
+            Owned by :{" "}
+            <span>{group.creator_first_name} {group.creator_last_name}</span>
+          </p>
+        </div>
+        <div className="bg-white shadow-xl h-[80px] w-6/12 rounded-lg p-4">
+          <div className=" text-purple-700 rounded-lg p-3 w-auto font-bold">
+            {group.description}
+          </div>
+        </div>
+        <div className="flex">
+          <GroupMembers group={group} />
+          <InviteComponent group={group} user={user?.uuid} />
         </div>
       </div>
-      <div className="flex">
-        <GroupMembers group={group} />
-        <InviteComponent group={group} user={user?.uuid} />
+      <div className="flex flex-wrap flex-row justify-around mt-8 bg-white rounded-lg shadow-lg w-full h-auto p-5">
+        {
+          //FIXME: no events
+          group.events?.map((event, index) => {
+            return <EventCard event={event} key={index} user={user?.uuid} />;
+          })
+        }
       </div>
     </div>
   );
--- services/frontend/app/ui/profile/toggle.tsx
+++ services/frontend/app/ui/profile/toggle.tsx
@@ -30,7 +30,6 @@ export default function Toggle({ children, className, user}: ToggleProps) {
         <div className="flex flex-wrap justify-around mt-5 items-center p-4">
             <label className={clsx("inline-flex items-center cursor-pointer", className)}>
                 {children === 'private' ? <input type="checkbox" className="sr-only peer" onChange={(event) => { apply(children, event) }} /> : <input type="checkbox" className="sr-only peer" defaultChecked onChange={(event) => { apply(children, event) }} />}
-                {children === 'private' ? <input type="checkbox" className="sr-only peer" onChange={(event) => { apply(children, event) }} /> : <input type="checkbox" className="sr-only peer" defaultChecked onChange={(event) => { apply(children, event) }} />}
                 <div className={`relative w-11 h-6 bg-gray-400 peer-focus:outline-none rounded-full peer dark:bg-black-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-purple-600`}></div>
                 <span className="ms-3 text-sm font-medium text-gray-900 dark:text-gray-400">
                     {child}
